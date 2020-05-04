3dc597ff6364811c69ba051a48acdc5a68d06760
Ye Kuang <yekuang.ky@gmail.com>
2020-05-04 10:20:03 +0900

    hello taichi

--- static/posts/2020-04-02-write-a-performant-ray-tracer-in-python-i/box.py
+++ static/posts/2020-04-02-write-a-performant-ray-tracer-in-python-i/box.py
@@ -1,18 +1,21 @@
 import taichi as ti
 
-ti.init(arch=ti.gpu)
 sz = 800
 res = (sz, sz)
+
+ti.init(arch=ti.gpu)
 color_buffer = ti.Vector(3, dt=ti.f32, shape=res)
+
 
 @ti.kernel
 def render():
-  for u, v in color_buffer:
-    color_buffer[u, v] = ti.Vector([0.678, 0.063, v / sz])
+    for u, v in color_buffer:
+        color_buffer[u, v] = ti.Vector([0.678, 0.063, v / sz])
+
 
 gui = ti.GUI('Cornell Box', res)
 for i in range(50000):
-    render()    
+    render()
     img = color_buffer.to_numpy(as_vector=True)
     gui.set_image(img)
     gui.show()
